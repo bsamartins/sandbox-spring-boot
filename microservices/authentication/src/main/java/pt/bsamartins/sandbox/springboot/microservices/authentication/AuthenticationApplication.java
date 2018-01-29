45ea01eba66769779ca9605cc946ff701de5a682
bmartins <bsamartins@gmail.com>
2018-01-29 18:25:35 +0000

    no message

--- microservices/authentication/src/main/java/pt/bsamartins/sandbox/springboot/microservices/authentication/AuthenticationApplication.java
+++ microservices/authentication/src/main/java/pt/bsamartins/sandbox/springboot/microservices/authentication/AuthenticationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableAuthorizationServer
-class AuthenticationApplication {
+public class AuthenticationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
     }
